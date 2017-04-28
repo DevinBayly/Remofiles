126ccbfdadb4efa631fc916d5a6161514daa3bf0
DevinBayly <baylyd@email.arizona.edu>
2017-04-28 07:31:45 -0700

    created dist1 and dist2 for eventual fitting formula, and added a error condition if the distingushers are ever > 1

--- R/encapsulated_data_build.R
+++ R/encapsulated_data_build.R
@@ -18,15 +18,19 @@
 build_data <- function(basedata,ID,Dyad,obs,mod,Dist,time_name) {
     ## naming of parameters has case selection to avoid prexisting variable
     ## names, appologies
-    basedata  <- data.frame("ID" = basedata[[ID]],"Dyad" = basedata[[Dyad]],"obs" =basedata[[obs]],"time" = basedata[[time_name]],"mod" = basedata[[mod]],"Dist" = basedata[[Dist]])
+    basedata  <- data.frame("ID" = basedata[[ID]],"Dyad" = basedata[[Dyad]],"obs" =basedata[[obs]],"time" = basedata[[time_name]],"mod" = basedata[[mod]],"Dist1" = basedata[[Dist]])
     ## filter out the exitsing NA's
     basedata  <- basedata %>%
         filter(!(is.na(obs)))
     factor_cols <- names(Filter(is.factor,basedata))
     for (col in names(Filter(is.factor,basedata))) {
       basedata[[col]] <- as.numeric(basedata[[col]])
+      #check to see if there is a number greater than 1 for either dist
+      if ((length(trimmed_data$Dist2) + length(trimmed_data$Dist1)) > 1) {
+        stop("Distinguisher Conversion from factor failed, please address in code")
+      }
     }
-    basedata["Dist_p"] = as.numeric(!basedata$Dist)
+    basedata["Dist2"] = as.numeric(!basedata$Dist)
     ## plot for people and pause for them to move on with the process
     print(chec_data_trellis(basedata))
     ## change original data to the changed df
