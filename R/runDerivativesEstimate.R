76f16b8bff9309b9d25bcb7d6905b49d2de67287
DevinBayly <baylyd@email.arizona.edu>
2017-05-21 17:21:40 -0700

    adapted choose derivatives to calculate each of these estimates on singel individual at a time, andgive us back the result as a very large dataframe

--- R/runDerivativesEstimate.R
+++ R/runDerivativesEstimate.R
@@ -1,4 +1,5 @@
 
+
 #' Title
 #'
 #' @param dat
@@ -10,16 +11,33 @@
 #'
 #' @examples
 choose_tau_dim <- function (dat,taus,dims) {
-
-
-    cat("freq,R^2,tau,dim\n")
-    for( tau in taus) {
-        for (dm in dims) {
-            dat  <- runDerivativesEstimate(1,dm,tau,dat)
-        }
+  ## fastest way to do this is to preallocate the amount of space that we need, I think we can count on the fact that there will always be less estimated rows than the
+  ret_df <- data.frame(matrix(0,ncol=length(taus)*length(dims)*3,nrow=length(dat$obs)))
+  ret_df_names <- c()
+  ## and then we can just fill in after
+    # cat("freq,R^2,tau,dim\n")
+  # we will move the pos to the right row position to insert the next individual's data
+  col_pos <- 1
+  row_pos <- 1
+  ## think of a way to use the dplyr grouping to give use the individual data we need
+  for( tau in taus) {
+    for (dm in dims) {
+      ind.est.data <- runDerivativesEstimate(1,dm,tau,dat)
+      ## ind.est.data will have several columns, each same len, now place in the matrix
+      size <- length(ind.est.data[,1])
+      ret_df[row_pos:(row_pos+size-1),
+             col_pos:(col_pos+2)] <- ind.est.data
+      ret_df_names <- c(ret_df_names,
+                        paste("resids",tau,dm,sep="_"),
+                        paste("d_resids",tau,dm,sep = "_"),
+                        paste("d2_resids",tau,dm,sep="_"))
+      col_pos =col_pos+3
     }
-  print("finished estimates")
-    return(dat)
+  }
+  #substitute in the colnames that we created
+  colnames(ret_df) <- ret_df_names
+
+  return(ret_df)
 }
 
 
@@ -38,6 +56,8 @@ choose_tau_dim <- function (dat,taus,dims) {
 #' @examples
 runDerivativesEstimate = function (deltaTime,theEmbed,theTau,dat_param) {
 
+  print(dat_param$ID[1])
+
     obsMatrix <- gllaEmbed(dat_param$resids, embed = theEmbed, tau = theTau)
 
     # Calculate Local Linear Approximation of derivatives
@@ -50,51 +70,29 @@ runDerivativesEstimate = function (deltaTime,theEmbed,theTau,dat_param) {
         order = 2
       )
     obsMatrixLLA <- obsMatrix[, 2:dim(obsMatrix)[2]] %*% wMatrix
+    return(as.data.frame(obsMatrixLLA))
 
-    ## conversion to df just makes this technique of adding rows easier
-    dfobsMatLLA  <- as.data.frame(obsMatrixLLA)
-    ## and now add an empty row at the top and the bottom.
-    dfobsMatLLA[seq(2,nrow(obsMatrixLLA)+1),]  <- dfobsMatLLA[seq(1,nrow(obsMatrixLLA)),]
-    dfobsMatLLA[1,]  <- NA ## make the first row NA
-    ## now add correct padding for end of dataframe
-    dfobsMatLLA[nrow(dfobsMatLLA) +(theEmbed -1)*theTau-1,]  <- NA
-    ## the embed -2 tells us that the wrapping by embed degree changes the number of rows dif in length our dataframe isfrom the matLLA, this is teh value we need to pad by
-
-
-
-    ## this is where the actual tau and embed labels would get attached
-    ## adding in the derivative columns
-    dat_param  <- dat_param %>%
-    mutate(d_resids = dfobsMatLLA[,2],
-           d2_resids = dfobsMatLLA[,3])
-
-  # Fit the damped linear oscillator model to get estimate of which settings give best derivative estimates
-
-    ## question, this is just for the one person right? doesn't involve the partner? it seems?
-    ## do we still need the r^2 stuff we are keeping all the variations...
   # library(nlme)
-  treg_self <-
-    lme(
-      d2_resids ~ resids + d_resids  - 1,
-      random = list(~resids + d_resids - 1 | ID),
-      data = dat_param,
-      control = lmeControl(opt = "optim"),
-      na.action = na.exclude
-    )
-  if(treg_self$coefficients$fixed[1] > 0) {
-    tLambda_self <- NA
-  } else {
-    tLambda_self <- 2 * pi / sqrt(-(treg_self$coefficients$fixed[1]))
-  }
-  ## is this the right formula for the R^2?
-  ## the ind helps make sure that only non NA values get used in the var
-  ind <- !is.na(dat_param$d2_resids)
-  Rsq <- round(1 - (var(treg_self$residuals[,2]) / var(dat_param$d2_resids[ind])),4)
-  cat(tLambda_self,Rsq,theTau,theEmbed,"\n")
-  ## and now we have to rename the columns
-
-  ## it goes tau then dim ok??
-  names(dat_param)[names(dat_param) == 'd_resids'] <- paste('d_resids_',theTau,'_',theEmbed,sep='')
-  names(dat_param)[names(dat_param) == 'd2_resids'] <- paste('d2_resids_',theTau,'_',theEmbed,sep='')
-  return(dat_param)
+  # treg_self <-
+  #   lme(
+  #     d2_resids ~ resids + d_resids  - 1,
+  #     random = list(~resids + d_resids - 1 | ID),
+  #     data = dat_param,
+  #     control = lmeControl(opt = "optim"),
+  #     na.action = na.exclude
+  #   )
+  # if(treg_self$coefficients$fixed[1] > 0) {
+  #   tLambda_self <- NA
+  # } else {
+  #   tLambda_self <- 2 * pi / sqrt(-(treg_self$coefficients$fixed[1]))
+  # }
+  # ## is this the right formula for the R^2?
+  # ## the ind helps make sure that only non NA values get used in the var
+  # ind <- !is.na(dat_param$d2_resids)
+  # Rsq <- round(1 - (var(treg_self$residuals[,2]) / var(dat_param$d2_resids[ind])),4)
+  # cat(tLambda_self,Rsq,theTau,theEmbed,"\n")
+  # ## and now we have to rename the columns
+  #
+  # ## it goes tau then dim ok??
+  # return(dat_param)
 }
--- scrap.r
+++ scrap.r
@@ -1,200 +1,57 @@
 ## these things are technically supposed to be taken care of
 ##I need to figure out if the previous code wasn't fully made generic
 
-test_names <- function (dat,name) {
-    return(dat[[ name ]][1:5])
-}
-
-## removing factor
-for (col in names(Filter(is.factor,trimmed_data))) {
-  trimmed_data[[col]] <- as.numeric(trimmed_data[[col]])
-  ## how to deal with cases where factor doesn't become 1s and 0s
-}
-
-taus <- c(1,2)
-dims <- c(5,6,7)
-basedat = read.csv("conv2secMods05.csv")# this is the importing step for the data
-trimmed_data  <- build_data(basedat,"Person","Couple","IBI","sexsat","female","timeCont")
-crossed_data <- process_data(trimmed_data,taus,dims)
-for (tau in taus) {
-  for (dim in dims) {
-    print(moderator_co(crossed_data,tau,dim)   )
-  }
-}
+library(dplyr)
+source("R/GLLAfunctions.R")
 
 
-## practice setting strings as formulas
-library(nlme)
-tau <- 1
-dim <- 5
-d2 <- paste("d2_resids",tau,dim,sep = "_")
-d <- paste("d_resids",tau,dim,sep = "_")
-dp <- paste("d_resids",tau,dim,"p",sep = "_")
-fitstring <-paste(d2,
-             paste("Dist1:resids + Dist0:resids + Dist1:",d," + Dist0:",d," + Dist1:resids_p +Dist0:resids_p + Dist1:",dp, " + Dist0:",dp," - 1",sep = "")
-             ,sep = " ~ ")
-fit_fmla <- as.formula(fitstring)
-randstring <- paste(" ~ resids + ",d," + resids_p + ",dp, " - 1 | Dyad",sep = "")
-rand_fmla <- as.formula(randstring)
+basedat_physio <- read.csv("../findingbug/physio2.csv")
+# basedat = read.csv("conv2secMods05.csv")# this is the importing step for the data
 
-fit <- lme(fit_fmla,random=list(rand_fmla), data=crossed_data, method="ML",na.action=na.omit,control=lmeControl(opt="optim"))
-summary(fit)
+trimmed_data  <- build_data(basedat_physio,"id","dyad","dial","diastolic","sexm","time")
+choose_tau_dim(trimmed_data,c(1),c(2))
+drop_shorties <- trimmed_data %>%
+  group_by(ID) %>%
+  filter(length(ID)>15)
 
-## now same process applide to the fitting for moderated version
-tau <- 1
-dim <- 5
-d2 <- paste("d2_resids",tau,dim,sep = "_")
-d <- paste("d_resids",tau,dim,sep = "_")
-dp <- paste("d_resids",tau,dim,"p",sep = "_")
-fitstring <-paste(d2," ~ Dist0:resids + Dist1:resids + Dist0:", d ,"  + Dist1:", d ,"  + Dist0:resids_p + Dist1:resids_p + Dist0:", dp ,"  + Dist1:", dp ,"  + Dist0:mod + Dist1:mod + Dist0:resids:mod + Dist1:resids:mod + Dist0:", d ," :mod + Dist1:", d ," :mod + Dist0:resids_p:mod + Dist1:resids_p:mod + Dist0:", dp ," :mod + Dist1:", dp ," :mod - 1")
-fitfmla <- as.formula(fitstring)
-fitfmla
-randomstring <- paste("~ resids + ", d ,"  + resids_p + ", dp ,"   - 1 | Dyad")
-randfmla <- as.formula(randomstring)
-randfmla
-library(nlme)
-fit <- lme(fitfmla,  random=list(randfmla), data=crossed_data, method="ML",na.action=na.omit,control=lmeControl(opt="optim"))
+##experimenting with the creating of the strings to make the column headers need tehh assign function to make this easier
+taus <- c(1,2,3)
+dims <- c(4,5,6)
 
-d2 ~ Dist0:resids + Dist1:resids + Dist0:d_resids_1_3 + Dist1:d_resids_1_3 + Dist0:resids_p + Dist1:resids_p + Dist0:d_resids_1_3_p + Dist1:d_resids_1_3_p + Dist0:mod + Dist1:mod + Dist0:resids:mod + Dist1:resids:mod + Dist0:d_resids_1_3:mod + Dist1:d_resids_1_3:mod + Dist0:resids_p:mod + Dist1:resids_p:mod + Dist0:d_resids_1_3_p:mod + Dist1:d_resids_1_3_p:mod - 1,  random=list(~ resids + d_resids_1_3 + resids_p + d_resids_1_3_p  - 1 | Dyad
+## making datafram from matrix, and then adding rows and columns
+test_df <- data.frame(matrix(0,nrow=length(trimmed_data$ID),ncol=3))
+#cehcking options for grouping and returning results to the test_DF
+residualized <- makeResidCol(trimmed_data)
+droped_residualized <- makeResidCol(drop_shorties)
+droped_residualized %>%
+  group_by(ID) %>%
+  do(choose_tau_dim(.,c(1,2),c(5,6)))
 
-fit <- lme(d2_resids_1_3 ~ Dist2:resids + Dist1:resids + Dist2:d_resids_1_3 + Dist1:d_resids_1_3 + Dist2:resids_p + Dist1:resids_p + Dist2:d_resids_1_3_p + Dist1:d_resids_1_3_p + Dist2:mod + Dist1:mod + Dist2:resids:mod + Dist1:resids:mod + Dist2:d_resids_1_3:mod + Dist1:d_resids_1_3:mod + Dist2:resids_p:mod + Dist1:resids_p:mod + Dist2:d_resids_1_3_p:mod + Dist1:d_resids_1_3_p:mod - 1,  random=list(~ resids + d_resids_1_3 + resids_p + d_resids_1_3_p  - 1 | Dyad), data=data, method="ML",na.action=na.omit,control=lmeControl(opt="optim"))
+colnames(residualized)
 
-
-####
+residualized %>%
+  group_by(ID) %>%
+  do(runDerivativesEstimate(1,5,2,.))
 
 
 
+theTau = 2
+theEmbed = 5
+residualized %>%
+  filter(ID == 1) -> dat_param
 
-basic_co(crossed_data,1,1)
-partnered_co(crossed_data,1,1)
-moderator_co(crossed_data,1,1)
-View(head(crossed_data))
+residualized %>%
+  group_by(ID) %>%
+  do(choose_tau_dim(.,c(2),c(5)))
 
+pid_two_est <- choose_tau_dim(residualized,c(2),c(5))
 
-## this is practice for the process of calculating the initial values for the osc model
-library(nlme)
-modLL <- lme(obs ~ 0 + Dist1 + Dist2 + Dist1:time + Dist2:time + Dist1:lowModResid + Dist2:lowModResid + Dist1:time:lowModResid + Dist2:time:lowModResid ,
-             data=crossed_data,
-             random = ~ 0 + Dist1 + Dist2 + Dist1:time + Dist2:time | Dyad  ,
-             method="ML",
-             na.action=na.omit,
-             control=lmeControl(opt="optim"))
-
-modLL_dist1 <- modLL$coefficients$fixed[1]
-modLL_dist2 <- modLL$coefficients$fixed[2]
-partner_fit <- partnered_co(crossed_data,1,1)
-partner_fit$coefficients$fixed
+plot(derivatives_dial[1:24,2] - pid_two_est[1:24,1])
+plot(derivatives_dial[1:24,3] - pid_two_est[1:24,2])
+plot(derivatives_dial[1:24,4] - pid_two_est[1:24,3])
 
 
-library(deSolve)
+## checking estimates for second id
 
-t <- seq(0,100,by = .01)
-pars <- c(eta = 0.5,zeta = 1.0)
-init <- c(x = 1.0,y= 0)
-
-prac <- function(time,state,params) {
-  with(
-    as.list(c(state,params)),{
-      ## here's the formula line
-      return(list(c(y,-eta*x + zeta*y)))
-    }
-  )
-}
-
-res <- ode(y = init,parms = pars,time = t,func = prac)
-
-invert <- function(x) {
-  return (1/x)
-}
-#
-# p <- ggplot(data = as.data.frame(res),aes(time,y)) + geom_point()  + scale_y_continuous(labels = function(x))
-# print(p)
-
-
-### plugging in for actual formula
-library(deSolve)
-t <- seq(0,100,by =.5)
-paramms <- c(dst1_resids = unname(partner_fit$coefficients$fixed[1]),
-           dst1_freq = unname(partner_fit$coefficients$fixed[3]),
-           dst2_resids_p = unname(partner_fit$coefficients$fixed[6]),
-           dst2_freq_p = unname(partner_fit$coefficients$fixed[8]))
-
-## the y and the z are the changes in the first two values.
-state <- c(p = unname(modLL_dist1),n = unname(modLL_dist2),y = 0,z= 0)
-
-modLL_dist2
-
-cplosc <- function(time,state,pars) {
-  gamp<-(dst1_resids - dst2_resids_p)
-  gamn<-(dst2_resids_p - dst1_resids)
-  with (
-    as.list(c(state,pars)), {
-      dy = dst1_freq*p + gamp*y + (dst1_freq*n  + dst2 )
-      dz  = dst2_freq_p*n + gamn*z + ()
-      return(list(c(y,1,dy,1)))
-    }
-  )
-}
-
-res <- ode(y = state,parms = paramms, func = cplosc,time = t)
-View(as.data.frame(res))
-library(ggplot2)
-p <- ggplot(data = as.data.frame(res),aes(time)) + geom_point(aes(y = y),color="green") + geom_point(aes(y = n),color="blue") + geom_point(aes(y = z),color="red") + geom_point(aes(y = p),color="purple")
-print(p)
-### using theirs as guide
-parameters <- c(dst1_freq = -8/3,
-                b = -10,
-                c = 28)
-state <- c( X = 1,
-            Y = 1,
-            Z = 1)
-
-lor <- function(time,state,parameters) {
-  with(as.list(c(state,parameters)),{
-    dx <- dst1_freq*X + Y*Z
-    dy <- b*(Y-Z)
-    dz <- -X*Y + c*Y -Z
-    list(c(dx,dy,dz))
-  })
-}
-
-t <- seq(0,100,by =.01)
-
-res <- ode(y = state,parms = parameters,func = lor,times = t)
-
-
-
-#debugging commands
-
-basedata %>%
-  filter(ID == 501) %>%
-  mutate( dist0= as.numeric(Dist1))
-
-
-## this is the space fortesting the centering, and residual, and mod column stuf,z
-
-## oh yea, the resids from the obs must be cleaned out
-
-resid_df  <- makeResidCol(basedat)
-dyadlabs  <- factor(resid_df$Dyad,levels = unique(resid_df$Dyad),labels <- unique(resid_df$Dyad))
-resid_df %>%
-    group_by(Dyad) %>%
-    xyplot(resids~time|dyadlabs,data = .,groups=Dist,type="l",main=.$Dyad)
-
-## I think maybe the last runderiv was used after the cross happened, but that
-## seems like more work, so lets just rewrite the thing making use of the dplyr
-## stuff again
-
-source("./GLLAfunctions.R")
-
-
-## testing whether we can go through multiple tau, dim things
-
-outDf  <- choose_tau_dim(resid_df)
-write.table(outDf,file = "success_emily_review.csv",sep = ',',append= FALSE)
-# seems to have worked over the different values!!!
-
-## test the cross on the full data
-crossed_dat  <- crossActorPartner(outDf)
-## incremental testing of cross
+## trying to switch the estimate process to person by person, seems to be a big difference between current method and the previous version
 
